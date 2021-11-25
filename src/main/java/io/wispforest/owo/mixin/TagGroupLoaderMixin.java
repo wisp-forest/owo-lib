84e877e9ec949c32e80b1df047878156f794416e
glisco <glisco03@web.de>
2021-11-25 21:46:44 +0100

    fix TagInjector acting globally on all tag types because im dum

--- src/main/java/io/wispforest/owo/mixin/TagGroupLoaderMixin.java
+++ src/main/java/io/wispforest/owo/mixin/TagGroupLoaderMixin.java
@@ -5,7 +5,9 @@ import net.minecraft.resource.ResourceManager;
 import net.minecraft.tag.Tag;
 import net.minecraft.tag.TagGroupLoader;
 import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -15,12 +17,18 @@ import java.util.Map;
 @Mixin(TagGroupLoader.class)
 public class TagGroupLoaderMixin {
 
+    @Shadow
+    @Final
+    private String dataType;
+
     @Inject(method = "loadTags", at = @At("TAIL"))
     public void injectValues(ResourceManager manager, CallbackInfoReturnable<Map<Identifier, Tag.Builder>> cir) {
         var map = cir.getReturnValue();
 
-        TagInjector.ADDITIIONS.forEach((identifier, identifiers) -> {
-            var builder = map.computeIfAbsent(identifier, identifier1 -> new Tag.Builder());
+        TagInjector.ADDITIIONS.forEach((location, identifiers) -> {
+            if (!this.dataType.equals(location.type())) return;
+
+            var builder = map.computeIfAbsent(location.tagId(), identifier1 -> new Tag.Builder());
             identifiers.forEach(addition -> builder.add(addition, "owo"));
         });
     }
--- src/main/java/io/wispforest/owo/util/TagInjector.java
+++ src/main/java/io/wispforest/owo/util/TagInjector.java
@@ -12,7 +12,14 @@ import java.util.*;
  */
 public class TagInjector {
 
-    public static final HashMap<Identifier, Set<Identifier>> ADDITIIONS = new HashMap<>();
+    public static final String BLOCK_TAG = "blocks";
+    public static final String ITEM_TAG = "items";
+    public static final String ENTITY_TAG = "entity_types";
+    public static final String FUNCTION_TAG = "functions";
+    public static final String GAME_EVENT_TAG = "game_events";
+    public static final String FLUID_TAG = "fluids";
+
+    public static final HashMap<TagLocation, Set<Identifier>> ADDITIIONS = new HashMap<>();
 
     /**
      * Injects the given Identifiers into the given Tag.
@@ -20,41 +27,50 @@ public class TagInjector {
      * relevant Registry, you <i>will</i> break the Tag.
      * If the Tag does not exist, it will be created.
      *
-     * @param tag    The tag to insert into, this could contain all kinds of values
-     * @param values The values to insert
+     * @param tagType The type of tag to inject. This essentially corresponds to the tag file path,
+     *                so {@code fluids} would be {@code data/<namespace>/tags/fluids}
+     * @param tag     The tag to insert into, this could contain all kinds of values
+     * @param values  The values to insert
      */
-    public static void injectRaw(Identifier tag, Collection<Identifier> values) {
-        ADDITIIONS.computeIfAbsent(tag, identifier -> new HashSet<>()).addAll(values);
+    public static void injectRaw(String tagType, Identifier tag, Collection<Identifier> values) {
+        ADDITIIONS.computeIfAbsent(new TagLocation(tagType, tag), identifier -> new HashSet<>()).addAll(values);
     }
 
-    public static void injectRaw(Identifier tag, Identifier... values) {
-        injectRaw(tag, Arrays.asList(values));
+    public static void injectRaw(String tagType, Identifier tag, Identifier... values) {
+        injectRaw(tagType, tag, Arrays.asList(values));
     }
 
     /**
      * A convenience method for directly inserting Blocks into the given Tag.
-     * If any of Blocks are not registered yet, this <i>will</i> throw an
+     * If any of the Blocks are not registered yet, this <i>will</i> throw an
      * exception.
      */
     public static void injectBlocks(Identifier tag, Collection<Block> values) {
-        injectRaw(tag, values.stream().map(Registry.BLOCK::getId).toList());
+        injectRaw(BLOCK_TAG, tag, values.stream().map(Registry.BLOCK::getId).toList());
     }
 
     public static void injectBlocks(Identifier tag, Block... values) {
-        injectRaw(tag, Arrays.stream(values).map(Registry.BLOCK::getId).toList());
+        injectRaw(BLOCK_TAG, tag, Arrays.stream(values).map(Registry.BLOCK::getId).toList());
     }
 
     /**
      * A convenience method for directly inserting Items into the given Tag.
-     * If any of Items are not registered yet, this <i>will</i> throw an
+     * If any of the Items are not registered yet, this <i>will</i> throw an
      * exception.
      */
     public static void injectItems(Identifier tag, Collection<Item> values) {
-        injectRaw(tag, values.stream().map(Registry.ITEM::getId).toList());
+        injectRaw(ITEM_TAG, tag, values.stream().map(Registry.ITEM::getId).toList());
     }
 
     public static void injectItems(Identifier tag, Item... values) {
-        injectRaw(tag, Arrays.stream(values).map(Registry.ITEM::getId).toList());
+        injectRaw(ITEM_TAG, tag, Arrays.stream(values).map(Registry.ITEM::getId).toList());
+    }
+
+    public static record TagLocation(String type, Identifier tagId) {
+        public TagLocation(String type, Identifier tagId) {
+            this.type = "tags/" + type;
+            this.tagId = tagId;
+        }
     }
 
 }
