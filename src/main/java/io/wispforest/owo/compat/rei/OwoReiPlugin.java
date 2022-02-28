1bf202d063633c88a19d718a40f0b6d59c8d9a88
glisco03 <glisco03@web.de>
2022-02-28 13:19:19 +0100

    re-enable REI plugin

--- src/main/java/io/wispforest/owo/compat/rei/OwoReiPlugin.java
+++ src/main/java/io/wispforest/owo/compat/rei/OwoReiPlugin.java
@@ -1,8 +1,18 @@
 package io.wispforest.owo.compat.rei;
 
-public class OwoReiPlugin /*implements REIClientPlugin*/ {
+import io.wispforest.owo.itemgroup.OwoItemGroup;
+import io.wispforest.owo.util.OwoCreativeInventoryScreenExtensions;
+import me.shedaniel.math.Rectangle;
+import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
+import me.shedaniel.rei.api.client.registry.screen.ExclusionZones;
+import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
+import net.minecraft.item.ItemGroup;
 
-    /*@Override
+import java.util.Collections;
+
+public class OwoReiPlugin implements REIClientPlugin {
+
+    @Override
     public void registerExclusionZones(ExclusionZones zones) {
         zones.register(CreativeInventoryScreen.class, screen -> {
             var group = ItemGroup.GROUPS[screen.getSelectedTab()];
@@ -17,5 +27,5 @@ public class OwoReiPlugin /*implements REIClientPlugin*/ {
 
             return Collections.singleton(new Rectangle(x + 200, y + 15, 28 + (28 * owoGroup.getButtons().size() / stackHeight), 25 * Math.min(owoGroup.getButtons().size(), owoGroup.getStackHeight())));
         });
-    }*/
+    }
 }
