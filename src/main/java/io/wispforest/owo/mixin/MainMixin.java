15d827194e64b4a6ae6b794e26380419a3567a05
glisco <glisco03@web.de>
2022-04-13 15:32:33 +0200

    fix quilt compatibility

--- src/main/java/io/wispforest/owo/mixin/MainMixin.java
+++ src/main/java/io/wispforest/owo/mixin/MainMixin.java
@@ -4,6 +4,7 @@ import io.wispforest.owo.util.OwoFreezer;
 import net.minecraft.server.Main;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Group;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
@@ -11,9 +12,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MainMixin {
 
     @SuppressWarnings({"MixinAnnotationTarget", "UnresolvedMixinReference"})
+    @Group(name = "serverFreezeHooks", min = 1, max = 1)
     @Inject(method = "main", at = @At(value = "INVOKE", remap = false,
             target = "Lnet/fabricmc/loader/impl/game/minecraft/Hooks;startServer(Ljava/io/File;Ljava/lang/Object;)V", shift = At.Shift.AFTER))
     private static void afterFabricHook(CallbackInfo ci) {
+        OwoFreezer.freeze();
+    }
+
+    @SuppressWarnings({"MixinAnnotationTarget", "UnresolvedMixinReference"})
+    @Group(name = "serverFreezeHooks", min = 1, max = 1)
+    @Inject(method = "main", at = @At(value = "INVOKE", remap = false,
+            target = "Lorg/quiltmc/loader/impl/game/minecraft/Hooks;startServer(Ljava/io/File;Ljava/lang/Object;)V", shift = At.Shift.AFTER))
+    private static void afterQuiltHook(CallbackInfo ci) {
         OwoFreezer.freeze();
     }
 
