c851d3aed8cfdd1e284eb8e5b50620f317c8b7ea
glisco <glisco03@web.de>
2022-05-29 21:03:00 +0200

    backport better OperatingSystem#open(URL) overwrite

--- src/main/java/io/wispforest/owo/mixin/tweaks/OperatingSystemMixin.java
+++ src/main/java/io/wispforest/owo/mixin/tweaks/OperatingSystemMixin.java
@@ -1,27 +1,41 @@
 package io.wispforest.owo.mixin.tweaks;
 
-import net.minecraft.client.MinecraftClient;
+import com.mojang.logging.LogUtils;
 import net.minecraft.util.Util;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.concurrent.CompletableFuture;
 
-@Mixin(Util.OperatingSystem.class)
+@Mixin(value = Util.OperatingSystem.class)
 public abstract class OperatingSystemMixin {
 
     @Shadow
-    public abstract void open(URL url);
+    protected abstract String[] getURLOpenCommand(URL url);
 
-    @Inject(method = "open(Ljava/net/URL;)V", at = @At("HEAD"), cancellable = true)
-    private void redirectOpenToWorker(URL url, CallbackInfo ci) {
-        if (MinecraftClient.getInstance().isOnThread()) {
-            CompletableFuture.runAsync(() -> this.open(url), Util.getMainWorkerExecutor());
-            ci.cancel();
-        }
+    /**
+     * @author glisco
+     * @reason By not properly consuming the stdout stream of the started process,
+     * Minecraft's implementation of this method causes xdg-open on linux to fail at actually
+     * opening the target program about 80% of the time. This overwrite uses a more modern approach
+     * to starting processes and properly voids both stdout and stderr, making xdg-open succeed
+     * at opening the user's desired application 100% of the time
+     */
+    @Overwrite()
+    public void open(URL url) {
+        CompletableFuture.runAsync(() -> {
+            try {
+                final var command = getURLOpenCommand(url);
+                new ProcessBuilder(command)
+                        .redirectError(ProcessBuilder.Redirect.DISCARD)
+                        .redirectOutput(ProcessBuilder.Redirect.DISCARD)
+                        .start();
+            } catch (IOException e) {
+                LogUtils.getLogger().error("Couldn't open url '{}'", url, e);
+            }
+        }, Util.getMainWorkerExecutor());
     }
 }
