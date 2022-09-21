828a2970aa67dff1a65021c3d84ad1ba41152694
glisco <glisco03@web.de>
2022-09-22 01:03:46 +0200

    disable remapping for RenderSystemMixin

--- src/main/java/io/wispforest/owo/mixin/ui/RenderSystemMixin.java
+++ src/main/java/io/wispforest/owo/mixin/ui/RenderSystemMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(RenderSystem.class)
+@Mixin(value = RenderSystem.class, remap = false)
 public class RenderSystemMixin {
 
     @Inject(method = "enableScissor", at = @At("HEAD"), cancellable = true)
