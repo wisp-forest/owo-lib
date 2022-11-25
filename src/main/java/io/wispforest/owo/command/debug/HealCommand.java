8383be7aa9137d2a505c6ee08866fb6324f57c39
chyzman <chyzalt@gmail.com>
2022-11-25 13:58:45 -0500

    made damage and heal commands more flexible

--- src/main/java/io/wispforest/owo/command/debug/HealCommand.java
+++ src/main/java/io/wispforest/owo/command/debug/HealCommand.java
@@ -7,6 +7,7 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import io.wispforest.owo.Owo;
 import io.wispforest.owo.ops.TextOps;
 import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
@@ -19,14 +20,50 @@ public class HealCommand {
 
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(literal("heal")
+                .executes(HealCommand::executeFullHeal)
+                .then(argument("amount", FloatArgumentType.floatArg(0))
+                        .executes(HealCommand::executeSelfHeal))
                 .then(argument("entity", EntityArgumentType.entity())
-                        .then(argument("amount", FloatArgumentType.floatArg(0)).executes(HealCommand::execute))));
+                        .executes(HealCommand::executeTargetedFullHeal)
+                        .then(argument("amount", FloatArgumentType.floatArg(0))
+                                .executes(HealCommand::executeTargetedHeal))));
     }
 
-    private static int execute(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-        final var entity = EntityArgumentType.getEntity(context, "entity");
-        final float amount = FloatArgumentType.getFloat(context, "amount");
+    private static int executeFullHeal(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+        var target = context.getSource().getEntityOrThrow();
+        return executeHeal(
+                context,
+                target,
+                target instanceof LivingEntity living ? living.getMaxHealth() : Float.MAX_VALUE
+        );
+    }
 
+    private static int executeSelfHeal(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+        return executeHeal(
+                context,
+                context.getSource().getEntityOrThrow(),
+                FloatArgumentType.getFloat(context, "amount")
+        );
+    }
+
+    private static int executeTargetedFullHeal(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+        var target = EntityArgumentType.getEntity(context, "entity");
+        return executeHeal(
+                context,
+                target,
+                target instanceof LivingEntity living ? living.getMaxHealth() : Float.MAX_VALUE
+        );
+    }
+
+    private static int executeTargetedHeal(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+        return executeHeal(
+                context,
+                EntityArgumentType.getEntity(context, "entity"),
+                FloatArgumentType.getFloat(context, "amount")
+        );
+    }
+
+    private static int executeHeal(CommandContext<ServerCommandSource> context, Entity entity, float amount) throws CommandSyntaxException {
         if (entity instanceof LivingEntity living) {
             float healed = living.getHealth();
             living.heal(amount);
