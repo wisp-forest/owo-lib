1696a1e90b0754111744a21497c547ca8231b7a9
glisco <glisco03@web.de>
2023-01-31 23:21:26 +0100

    [ui] re-add GreedyInputComponent interface to text field widgets

--- src/main/java/io/wispforest/owo/mixin/ui/TextFieldWidgetMixin.java
+++ src/main/java/io/wispforest/owo/mixin/ui/TextFieldWidgetMixin.java
@@ -1,5 +1,6 @@
 package io.wispforest.owo.mixin.ui;
 
+import io.wispforest.owo.ui.inject.GreedyInputComponent;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -7,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TextFieldWidget.class)
-public class TextFieldWidgetMixin {
+public abstract class TextFieldWidgetMixin implements GreedyInputComponent {
 
     @Inject(method = "onChanged", at = @At("HEAD"))
     private void callOwoListener(String newText, CallbackInfo ci) {
