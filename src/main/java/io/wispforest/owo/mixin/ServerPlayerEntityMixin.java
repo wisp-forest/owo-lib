90a8b6791141be9897356e83d51edfe8da9aa215
glisco <glisco03@web.de>
2023-04-26 18:49:10 +0200

    [config] fix & improve server-sided client option retrieval  - add safer version of ConfigSynchronizer.getClientOptions which directly accepts a config wrapper  - store client options per client connection instead of on the player object to avoid losing information on death  - provide option keys on generated config wrapper class to improve safety on server-sided value retrieval

--- src/main/java/io/wispforest/owo/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/io/wispforest/owo/mixin/ServerPlayerEntityMixin.java
@@ -1,29 +1,15 @@
 package io.wispforest.owo.mixin;
 
-import io.wispforest.owo.config.ConfigSynchronizer;
-import io.wispforest.owo.config.Option;
 import io.wispforest.owo.util.pond.OwoScreenHandlerExtension;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Mixin(ServerPlayerEntity.class)
-public class ServerPlayerEntityMixin implements ConfigSynchronizer.ServerPlayerEntityExtension {
-
-    @Unique
-    private final Map<String, Map<Option.Key, Object>> owo$optionStorage = new HashMap<>();
-
-    @Override
-    public Map<String, Map<Option.Key, Object>> owo$optionStorage() {
-        return this.owo$optionStorage;
-    }
+public class ServerPlayerEntityMixin {
 
     @SuppressWarnings("ConstantConditions")
     @Inject(method = "onScreenHandlerOpened", at = @At("HEAD"))
