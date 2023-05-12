97772c0a94c72fae7313ef3cbe9afbb6e310e209
Basique <basiqueevangelist@yandex.ru>
2023-05-12 16:08:31 +0300

    guys it's the fabric api

--- src/main/java/io/wispforest/owo/shader/GlProgram.java
+++ src/main/java/io/wispforest/owo/shader/GlProgram.java
@@ -2,20 +2,12 @@ package io.wispforest.owo.shader;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import io.wispforest.owo.mixin.shader.ShaderProgramAccessor;
+import net.fabricmc.fabric.api.client.rendering.v1.CoreShaderRegistrationCallback;
 import net.minecraft.client.gl.GlUniform;
 import net.minecraft.client.gl.ShaderProgram;
 import net.minecraft.client.render.VertexFormat;
-import net.minecraft.resource.ResourceFactory;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.Pair;
-import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
 
 /**
  * A simple wrapper around Minecraft's built-in core shaders. In order to load and use
@@ -33,8 +25,6 @@ import java.util.function.Function;
  */
 public class GlProgram {
 
-    private static final List<Pair<Function<ResourceFactory, ShaderProgram>, Consumer<ShaderProgram>>> REGISTERED_PROGRAMS = new ArrayList<>();
-
     /**
      * The actual Minecraft shader program
      * which is represented and wrapped by this
@@ -43,19 +33,12 @@ public class GlProgram {
     protected ShaderProgram backingProgram;
 
     public GlProgram(Identifier id, VertexFormat vertexFormat) {
-        REGISTERED_PROGRAMS.add(new Pair<>(
-                resourceFactory -> {
-                    try {
-                        return new OwoShaderProgram(resourceFactory, id.toString(), vertexFormat);
-                    } catch (IOException e) {
-                        throw new RuntimeException("Failed to initialized owo shader program", e);
-                    }
-                },
-                program -> {
-                    this.backingProgram = program;
-                    this.setup();
-                }
-        ));
+        CoreShaderRegistrationCallback.EVENT.register(context -> {
+            context.register(id, vertexFormat, program -> {
+                this.backingProgram = program;
+                this.setup();
+            });
+        });
     }
 
     /**
@@ -81,16 +64,5 @@ public class GlProgram {
      */
     protected @Nullable GlUniform findUniform(String name) {
         return ((ShaderProgramAccessor) this.backingProgram).owo$getLoadedUniforms().get(name);
-    }
-
-    @ApiStatus.Internal
-    public static void forEachProgram(Consumer<Pair<Function<ResourceFactory, ShaderProgram>, Consumer<ShaderProgram>>> loader) {
-        REGISTERED_PROGRAMS.forEach(loader);
-    }
-
-    public static class OwoShaderProgram extends ShaderProgram {
-        private OwoShaderProgram(ResourceFactory factory, String name, VertexFormat format) throws IOException {
-            super(factory, name, format);
-        }
     }
 }
