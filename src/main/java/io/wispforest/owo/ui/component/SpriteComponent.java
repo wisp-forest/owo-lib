6274609b8c8549e4dcfa01785cc3db863700c7c5
glisco <glisco03@web.de>
2024-05-26 01:28:01 +0200

    [ui] support blending in SpriteComponent

--- src/main/java/io/wispforest/owo/ui/component/SpriteComponent.java
+++ src/main/java/io/wispforest/owo/ui/component/SpriteComponent.java
@@ -4,15 +4,19 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import io.wispforest.owo.ui.base.BaseComponent;
 import io.wispforest.owo.ui.core.OwoUIDrawContext;
 import io.wispforest.owo.ui.core.Sizing;
+import io.wispforest.owo.ui.parsing.UIModel;
 import io.wispforest.owo.ui.parsing.UIParsing;
 import io.wispforest.owo.ui.util.SpriteUtilInvoker;
 import net.minecraft.client.texture.Sprite;
 import net.minecraft.client.util.SpriteIdentifier;
 import org.w3c.dom.Element;
 
+import java.util.Map;
+
 public class SpriteComponent extends BaseComponent {
 
     protected final Sprite sprite;
+    protected boolean blend = false;
 
     protected SpriteComponent(Sprite sprite) {
         this.sprite = sprite;
@@ -31,9 +35,33 @@ public class SpriteComponent extends BaseComponent {
     @Override
     public void draw(OwoUIDrawContext context, int mouseX, int mouseY, float partialTicks, float delta) {
         SpriteUtilInvoker.markSpriteActive(this.sprite);
+
+        if (this.blend) {
+            RenderSystem.enableBlend();
+            RenderSystem.defaultBlendFunc();
+        }
+
         context.drawSprite(this.x, this.y, 0, this.width, this.height, this.sprite);
+
+        if (this.blend) {
+            RenderSystem.disableBlend();
+        }
     }
 
+    public SpriteComponent blend(boolean blend) {
+        this.blend = blend;
+        return this;
+    }
+
+    public boolean blend() {
+        return this.blend;
+    }
+
+    @Override
+    public void parseProperties(UIModel model, Element element, Map<String, Element> children) {
+        super.parseProperties(model, element, children);
+        UIParsing.apply(children, "blend", UIParsing::parseBool, this::blend);
+    }
     public static SpriteComponent parse(Element element) {
         UIParsing.expectAttributes(element, "atlas", "sprite");
 
