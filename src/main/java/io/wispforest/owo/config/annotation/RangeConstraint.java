06ab48a8da5a9d01864123739104d7f90834a731
chyzman <32403637+chyzman@users.noreply.github.com>
2024-09-23 19:34:53 -0400

    made range constraint better (but its unfinished)

--- src/main/java/io/wispforest/owo/config/annotation/RangeConstraint.java
+++ src/main/java/io/wispforest/owo/config/annotation/RangeConstraint.java
@@ -1,5 +1,7 @@
 package io.wispforest.owo.config.annotation;
 
+import net.fabricmc.fabric.api.util.TriState;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -12,13 +14,28 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface RangeConstraint {
-    double min();
+    double min() default -Double.MAX_VALUE;
 
-    double max();
+    double max() default Double.MAX_VALUE;
 
     /**
      * @return How many decimals places to show in the config
      * screen, if this is a floating point option
      */
     int decimalPlaces() default 2;
+
+    /**
+     * @return Can this range be configured with a slider?
+     */
+    boolean allowSlider() default true;
+
+    /**
+     * @return the default option type for this range constraint
+     */
+    DefaultOptionType defaultOption() default DefaultOptionType.SLIDER;
+
+    enum DefaultOptionType {
+        SLIDER,
+        TEXT_BOX
+    }
 }
