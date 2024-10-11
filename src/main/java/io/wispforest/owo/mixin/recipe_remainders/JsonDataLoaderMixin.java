371a0d3f97c3a1329c4aea224ef3c9996e8f9d03
Dragon-Seeker <timekeeperguild@gmail.com>
2024-10-10 23:10:22 -0500

    Fix issues where returning null instead of the json element within JsonDataLoader

--- src/main/java/io/wispforest/owo/mixin/recipe_remainders/JsonDataLoaderMixin.java
+++ src/main/java/io/wispforest/owo/mixin/recipe_remainders/JsonDataLoaderMixin.java
@@ -29,23 +29,23 @@ public abstract class JsonDataLoaderMixin {
         var element = original.call(jsonReader);
 
         if (dataType.equals(RegistryKeys.getPath(RegistryKeys.RECIPE)) && element instanceof JsonObject json) {
-            if (!json.has("owo:remainders")) return null;
+            if (json.has("owo:remainders")) {
+                var remainders = new HashMap<Item, ItemStack>();
 
-            var remainders = new HashMap<Item, ItemStack>();
-            for (var remainderEntry : json.getAsJsonObject("owo:remainders").entrySet()) {
-                var item = JsonHelper.asItem(new JsonPrimitive(remainderEntry.getKey()), remainderEntry.getKey());
+                for (var remainderEntry : json.getAsJsonObject("owo:remainders").entrySet()) {
+                    var item = JsonHelper.asItem(new JsonPrimitive(remainderEntry.getKey()), remainderEntry.getKey());
 
-                if (remainderEntry.getValue().isJsonObject()) {
-                    var remainderStack = ItemStack.CODEC.parse(JsonOps.INSTANCE, remainderEntry.getValue().getAsJsonObject()).getOrThrow(JsonParseException::new);
-                    remainders.put(item.value(), remainderStack);
-                } else {
-                    var remainderItem = JsonHelper.asItem(remainderEntry.getValue(), "item");
-                    remainders.put(item.value(), new ItemStack(remainderItem));
+                    if (remainderEntry.getValue().isJsonObject()) {
+                        var remainderStack = ItemStack.CODEC.parse(JsonOps.INSTANCE, remainderEntry.getValue().getAsJsonObject()).getOrThrow(JsonParseException::new);
+                        remainders.put(item.value(), remainderStack);
+                    } else {
+                        var remainderItem = JsonHelper.asItem(remainderEntry.getValue(), "item");
+                        remainders.put(item.value(), new ItemStack(remainderItem));
+                    }
                 }
-            }
 
-            if (remainders.isEmpty()) return null;
-            RecipeRemainderStorage.store(recipeId, remainders);
+                if (!remainders.isEmpty()) RecipeRemainderStorage.store(recipeId, remainders);
+            }
         }
 
         return element;
