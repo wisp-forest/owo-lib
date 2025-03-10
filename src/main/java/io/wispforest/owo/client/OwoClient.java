ff4dbf059770d02278459937843ae31b3b0ffe36
Dragon-Seeker <timekeeperguild@gmail.com>
2025-03-10 15:29:34 -0500

    Revert changes to RenderDoc Code disabling

--- src/main/java/io/wispforest/owo/client/OwoClient.java
+++ src/main/java/io/wispforest/owo/client/OwoClient.java
@@ -63,18 +63,20 @@ public class OwoClient {
             event.registerReloadListener(new NinePatchTexture.MetadataLoader());
         });
 
-//        final var renderdocPath = System.getProperty("owo.renderdocPath");
-//        if (renderdocPath != null) {
-//            if (Util.getOperatingSystem() == Util.OperatingSystem.WINDOWS) {
-//                System.load(renderdocPath);
-//            } else {
-//                Owo.LOGGER.warn(switch (Util.getOperatingSystem()) {
-//                    case LINUX -> LINUX_RENDERDOC_WARNING;
-//                    case OSX -> MAC_RENDERDOC_WARNING;
-//                    default -> GENERIC_RENDERDOC_WARNING;
-//                });
-//            }
-//        }
+        if (Owo.DEBUG) {
+            final var renderdocPath = System.getProperty("owo.renderdocPath");
+            if (renderdocPath != null) {
+                if (Util.getOperatingSystem() == Util.OperatingSystem.WINDOWS) {
+                    System.load(renderdocPath);
+                } else {
+                    Owo.LOGGER.warn(switch (Util.getOperatingSystem()) {
+                        case LINUX -> LINUX_RENDERDOC_WARNING;
+                        case OSX -> MAC_RENDERDOC_WARNING;
+                        default -> GENERIC_RENDERDOC_WARNING;
+                    });
+                }
+            }
+        }
 
         ScreenInternals.Client.init();
 
