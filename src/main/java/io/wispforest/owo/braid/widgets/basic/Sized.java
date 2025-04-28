b90bf6bc69f4f2c1feb9ee92b7b4ffbd919ce266
chyzman <32403637+chyzman@users.noreply.github.com>
2025-04-28 15:39:35 -0400

    make sized better

--- src/main/java/io/wispforest/owo/braid/widgets/basic/Sized.java
+++ src/main/java/io/wispforest/owo/braid/widgets/basic/Sized.java
@@ -16,6 +16,14 @@ public class Sized extends ConstraintWidget {
         this.height = height;
     }
 
+    public Sized(@Nullable Integer width, @Nullable Integer height, Widget child) {
+        this(width == null ? null : width.doubleValue(), height == null ? null : height.doubleValue(), child);
+    }
+
+    public Sized(double width, double height, Widget child) {
+        this((Double) width, (Double) height, child);
+    }
+
     public Sized(Size size, Widget child) {
         this(size.width(), size.height(), child);
     }
