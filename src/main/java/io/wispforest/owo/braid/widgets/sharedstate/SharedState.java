c18d7fe281186c76c661d8151d75e43757a4633e
chyzman <32403637+chyzman@users.noreply.github.com>
2025-04-30 19:28:22 -0400

    the ***MOST*** volatile sharedstate

--- src/main/java/io/wispforest/owo/braid/widgets/sharedstate/SharedState.java
+++ src/main/java/io/wispforest/owo/braid/widgets/sharedstate/SharedState.java
@@ -1,10 +1,12 @@
 package io.wispforest.owo.braid.widgets.sharedstate;
 
+import com.google.common.base.Preconditions;
 import io.wispforest.owo.braid.framework.BuildContext;
 import io.wispforest.owo.braid.framework.proxy.WidgetState;
 import io.wispforest.owo.braid.framework.widget.StatefulWidget;
 import io.wispforest.owo.braid.framework.widget.Widget;
 
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class SharedState<T extends SharableState> extends StatefulWidget {
@@ -19,6 +21,28 @@ public class SharedState<T extends SharableState> extends StatefulWidget {
     @Override
     public WidgetState<SharedState<T>> createState() {
         return new State<>();
+    }
+
+    //TODO glisco fix this at some point
+    public static <T extends SharableState> T get(BuildContext context, Class<T> clazz) {
+        var provider = context.dependOnAncestor(SharedStateProvider.class);
+        Preconditions.checkArgument(provider != null, "attempted to read inherited state which is not provided by the current context");
+
+        //TODO its right there VVVV
+        return (T) provider.state.state;
+    }
+
+    //TODO also fix this one
+    public static <T extends SharableState> void set(BuildContext context, Class<T> clazz, Consumer<T> consumer) {
+        var provider = context.dependOnAncestor(SharedStateProvider.class);
+        Preconditions.checkArgument(provider != null, "attempted to set inherited state which is not provided by the current context");
+
+        //TODO this one is there tho --------VVVV
+        provider.state.setState(() -> {
+            consumer.accept((T) provider.state.state);
+            provider.state.generation++;
+        });
+
     }
 
     public static class State<T extends SharableState> extends WidgetState<SharedState<T>> {
