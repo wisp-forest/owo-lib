0a1e743d115e0c6b34cc25f682cf5aaff62e5420
chyzman <32403637+chyzman@users.noreply.github.com>
2025-06-09 16:53:48 -0400

    Buttons now use actions

--- src/main/java/io/wispforest/owo/braid/widgets/button/MessageButton.java
+++ src/main/java/io/wispforest/owo/braid/widgets/button/MessageButton.java
@@ -1,42 +1,29 @@
 package io.wispforest.owo.braid.widgets.button;
 
-import io.wispforest.owo.braid.core.Insets;
-import io.wispforest.owo.braid.core.cursor.CursorStyle;
 import io.wispforest.owo.braid.framework.BuildContext;
-import io.wispforest.owo.braid.framework.proxy.WidgetState;
-import io.wispforest.owo.braid.framework.widget.StatefulWidget;
 import io.wispforest.owo.braid.framework.widget.StatelessWidget;
 import io.wispforest.owo.braid.framework.widget.Widget;
-import io.wispforest.owo.braid.widgets.basic.MouseArea;
-import io.wispforest.owo.braid.widgets.basic.Padding;
-import io.wispforest.owo.braid.widgets.basic.Panel;
 import io.wispforest.owo.braid.widgets.label.Label;
 import io.wispforest.owo.braid.widgets.label.LabelStyle;
-import io.wispforest.owo.ui.component.ButtonComponent;
 import io.wispforest.owo.ui.core.Color;
-import io.wispforest.owo.ui.util.UISounds;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.function.BooleanSupplier;
 import java.util.function.IntPredicate;
 
 public class MessageButton extends StatelessWidget {
 
     public final Text text;
-    public final @Nullable IntPredicate onClick;
+    public final @Nullable Runnable onClick;
 
-    public MessageButton(Text text, @Nullable IntPredicate onClick) {
+    public MessageButton(Text text, @Nullable Runnable onClick) {
         this.text = text;
         this.onClick = onClick;
     }
 
-    public MessageButton(Text text, @Nullable Runnable onClick) {
-        this(text, onClick != null ? (x) -> {
-            onClick.run();
-            return true;
-        } : null);
+    public MessageButton(Text text, boolean active, @Nullable Runnable onClick) {
+        this(text, active ? onClick : null);
     }
 
     @Override
--- src/main/java/io/wispforest/owo/braid/widgets/button/RawButton.java
+++ src/main/java/io/wispforest/owo/braid/widgets/button/RawButton.java
@@ -4,36 +4,33 @@ import io.wispforest.owo.braid.core.cursor.CursorStyle;
 import io.wispforest.owo.braid.framework.BuildContext;
 import io.wispforest.owo.braid.framework.widget.StatelessWidget;
 import io.wispforest.owo.braid.framework.widget.Widget;
-import io.wispforest.owo.braid.widgets.basic.MouseArea;
+import io.wispforest.owo.braid.widgets.basic.action.Actions;
 import io.wispforest.owo.ui.util.UISounds;
 import org.jetbrains.annotations.Nullable;
-import java.util.function.IntPredicate;
 
 public class RawButton extends StatelessWidget {
 
-    public final @Nullable IntPredicate onClick;
+    public final @Nullable Runnable onClick;
     public final Widget child;
 
-    public RawButton(@Nullable IntPredicate onClick, Widget child) {
+    public RawButton(@Nullable Runnable onClick, Widget child) {
         this.onClick = onClick;
         this.child = child;
     }
 
-    public RawButton(IntPredicate onClick, boolean active, Widget child) {
+    public RawButton(Runnable onClick, boolean active, Widget child) {
         this(active ? onClick : null, child);
     }
 
     @Override
     public Widget build(BuildContext context) {
         if (this.onClick == null) return this.child;
-        return new MouseArea(
-            widget -> widget
-                .clickCallback((x, y, button) -> {
-                    var result = this.onClick.test(button);
-                    UISounds.playButtonSound();
-                    //TODO: once actions exist, return result here
-                })
-                .cursorStyle(CursorStyle.HAND),
+        return Actions.click(
+            widget -> widget.cursorStyle(CursorStyle.HAND),
+            () -> {
+                this.onClick.run();
+                UISounds.playButtonSound();
+            },
             child
         );
     }
