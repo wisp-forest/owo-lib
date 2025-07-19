228f1726a8be1c98f31f582d6ee16e054cb60810
Basique <basiqueevangelist@yandex.ru>
2025-07-19 23:16:17 +0300

    [testmod] make braid screens have the REI overlay

--- src/testmod/java/io/wispforest/uwu/rei/UwuReiPlugin.java
+++ src/testmod/java/io/wispforest/uwu/rei/UwuReiPlugin.java
@@ -1,8 +1,15 @@
 package io.wispforest.uwu.rei;
 
+import io.wispforest.owo.braid.core.BraidScreen;
+import me.shedaniel.math.Rectangle;
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
+import me.shedaniel.rei.api.client.registry.screen.DisplayBoundsProvider;
+import me.shedaniel.rei.api.client.registry.screen.ScreenRegistry;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.util.ActionResult;
+import org.jetbrains.annotations.Nullable;
 
 public class UwuReiPlugin implements REIClientPlugin {
 
@@ -16,4 +23,23 @@ public class UwuReiPlugin implements REIClientPlugin {
         registry.add(new UiCategory.UiDisplay());
     }
 
+    @Override
+    public void registerScreens(ScreenRegistry registry) {
+        registry.registerDecider(new DisplayBoundsProvider<BraidScreen>() {
+            @Override
+            public @Nullable Rectangle getScreenBounds(BraidScreen screen) {
+                return new Rectangle(screen.width / 4, screen.height / 4, screen.width / 2, screen.height / 2);
+            }
+
+            @Override
+            public <R extends Screen> boolean isHandingScreen(Class<R> screen) {
+                return BraidScreen.class.isAssignableFrom(screen);
+            }
+
+            @Override
+            public <R extends Screen> ActionResult shouldScreenBeOverlaid(R screen) {
+                return ActionResult.SUCCESS;
+            }
+        });
+    }
 }
