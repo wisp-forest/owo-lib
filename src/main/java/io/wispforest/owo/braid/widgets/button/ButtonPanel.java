01c5e519ed512489a1b7b7930cdbc89b42aefc95
chyzman <32403637+chyzman@users.noreply.github.com>
2025-07-23 18:33:22 -0400

    make ButtonPanel use Hoverable

--- src/main/java/io/wispforest/owo/braid/widgets/button/ButtonPanel.java
+++ src/main/java/io/wispforest/owo/braid/widgets/button/ButtonPanel.java
@@ -2,15 +2,14 @@ package io.wispforest.owo.braid.widgets.button;
 
 import io.wispforest.owo.braid.core.Insets;
 import io.wispforest.owo.braid.framework.BuildContext;
-import io.wispforest.owo.braid.framework.proxy.WidgetState;
-import io.wispforest.owo.braid.framework.widget.StatefulWidget;
+import io.wispforest.owo.braid.framework.widget.StatelessWidget;
 import io.wispforest.owo.braid.framework.widget.Widget;
-import io.wispforest.owo.braid.widgets.basic.MouseArea;
+import io.wispforest.owo.braid.widgets.basic.Hoverable;
 import io.wispforest.owo.braid.widgets.basic.Padding;
 import io.wispforest.owo.braid.widgets.basic.Panel;
 import io.wispforest.owo.ui.component.ButtonComponent;
 
-public class ButtonPanel extends StatefulWidget {
+public class ButtonPanel extends StatelessWidget {
     public final boolean active;
     public final Widget child;
 
@@ -20,29 +19,18 @@ public class ButtonPanel extends StatefulWidget {
     }
 
     @Override
-    public WidgetState<ButtonPanel> createState() {
-        return new State();
-    }
-
-    public static class State extends WidgetState<ButtonPanel> {
-        private boolean hovered = false;
-
-        @Override
-        public Widget build(BuildContext context) {
-            return new MouseArea(
-                widget -> widget
-                    .enterCallback(() -> this.setState(() -> this.hovered = true))
-                    .exitCallback(() -> this.setState(() -> this.hovered = false)),
+    public Widget build(BuildContext context) {
+        return new Hoverable(
+            hovered ->
                 new Panel(
-                    this.widget().active
-                        ? this.hovered ? ButtonComponent.HOVERED_TEXTURE : ButtonComponent.ACTIVE_TEXTURE
+                    this.active
+                        ? hovered ? ButtonComponent.HOVERED_TEXTURE : ButtonComponent.ACTIVE_TEXTURE
                         : ButtonComponent.DISABLED_TEXTURE,
                     new Padding(
                         Insets.all(5),
-                        this.widget().child
+                        this.child
                     )
                 )
-            );
-        }
+        );
     }
 }
