0725638829fb4979bc6411ec6e33a3be6dea84e8
glisco <glisco03@web.de>
2025-07-25 01:27:25 +0200

    (braid) port new `EditableText` changes

--- src/main/java/io/wispforest/owo/braid/widgets/textinput/EditableText.java
+++ src/main/java/io/wispforest/owo/braid/widgets/textinput/EditableText.java
@@ -1,18 +1,17 @@
 package io.wispforest.owo.braid.widgets.textinput;
 
-import io.wispforest.owo.braid.core.Constraints;
 import io.wispforest.owo.braid.framework.BuildContext;
 import io.wispforest.owo.braid.framework.instance.WidgetInstance;
 import io.wispforest.owo.braid.framework.proxy.WidgetState;
 import io.wispforest.owo.braid.framework.widget.StatefulWidget;
 import io.wispforest.owo.braid.framework.widget.Widget;
-import io.wispforest.owo.braid.widgets.basic.Constrain;
+import io.wispforest.owo.braid.widgets.basic.Builder;
 import io.wispforest.owo.braid.widgets.basic.KeyboardInput;
-import io.wispforest.owo.braid.widgets.basic.LayoutBuilder;
 import io.wispforest.owo.braid.widgets.basic.VisitorWidget;
 import io.wispforest.owo.braid.widgets.scroll.ScrollController;
 import io.wispforest.owo.braid.widgets.scroll.Scrollable;
 import net.minecraft.text.Style;
+import net.minecraft.util.math.Box;
 
 import java.time.Duration;
 import java.util.function.Consumer;
@@ -45,14 +44,11 @@ public class EditableText extends StatefulWidget {
         private static final Duration CURSOR_BLINK_INTERVAL = Duration.ofMillis(300);
         private boolean showCursor = false;
 
-        // TODO(glisco): rewrite once cancellable callbacks are merged
-        private boolean callbackScheduled = false;
-        private boolean blink = false;
+        private long blinkCallbackId = -1;
 
         private final ScrollController horizontalController = new ScrollController();
         private final ScrollController verticalController = new ScrollController();
-        private TextInput.Instance inputInstance;
-        private WidgetInstance<?> scrollableInstance;
+        private BuildContext inputContext;
 
         @Override
         public void init() {
@@ -74,58 +70,49 @@ public class EditableText extends StatefulWidget {
 
         private void listenerCallback() {
             this.schedulePostLayoutCallback(() -> {
-                if (this.inputInstance != null && this.scrollableInstance != null) {
-                    var cursorX = this.inputInstance.cursorPosition().x + 2;
-                    if (cursorX > this.scrollableInstance.transform.width() + this.horizontalController.offset()) {
-                        this.horizontalController.setOffset(cursorX - this.scrollableInstance.transform.width());
-                    }
+                var inputInstance = (TextInput.Instance) this.inputContext.instance();
+                var cursorPos = inputInstance.cursorPosition();
+                var lineHeight = inputInstance.host().client().textRenderer.fontHeight;
 
-                    cursorX -= 2;
-                    if (cursorX < this.horizontalController.offset()) {
-                        this.horizontalController.setOffset(cursorX);
-                    }
-
-                    if (this.widget().allowMultipleLines) {
-                        var cursorY = this.inputInstance.cursorPosition().y;
-                        if (cursorY > this.scrollableInstance.transform.height() + this.verticalController.offset()) {
-                            this.verticalController.setOffset(cursorY - this.scrollableInstance.transform.height());
-                        }
-
-                        cursorY -= this.inputInstance.host().client().textRenderer.fontHeight;
-                        if (cursorY < this.verticalController.offset()) {
-                            this.verticalController.setOffset(cursorY);
-                        }
-                    }
-                }
+                Scrollable.revealAabb(
+                    this.inputContext,
+                    new Box(
+                        cursorPos.x,
+                        cursorPos.y - lineHeight,
+                        0,
+                        cursorPos.x + 2,
+                        cursorPos.y,
+                        0
+                    )
+                );
             });
 
             this.restartBlinking();
         }
 
         private void restartBlinking() {
-            this.blink = true;
+            if (this.blinkCallbackId != -1) {
+                this.cancelDelayedCallback(this.blinkCallbackId);
+                this.blinkCallbackId = -1;
+            }
 
             this.setState(() -> this.showCursor = true);
 
-            if (!this.callbackScheduled) {
-                this.scheduleDelayedCallback(CURSOR_BLINK_INTERVAL, this::blink);
-                this.callbackScheduled = true;
-            }
+            this.blinkCallbackId = this.scheduleDelayedCallback(CURSOR_BLINK_INTERVAL, this::blink);
         }
 
         private void stopBlinking() {
-            this.blink = false;
+            if (this.blinkCallbackId != -1) {
+                this.cancelDelayedCallback(this.blinkCallbackId);
+                this.blinkCallbackId = -1;
+            }
+
             this.setState(() -> this.showCursor = false);
         }
 
         private void blink() {
-            if (!this.blink) {
-                this.callbackScheduled = false;
-                return;
-            }
-
             this.setState(() -> this.showCursor = !this.showCursor);
-            this.scheduleDelayedCallback(CURSOR_BLINK_INTERVAL, this::blink);
+            this.blinkCallbackId = this.scheduleDelayedCallback(CURSOR_BLINK_INTERVAL, this::blink);
         }
 
         @Override
@@ -134,30 +121,22 @@ public class EditableText extends StatefulWidget {
                 widget -> widget
                     .focusGainedCallback(this::restartBlinking)
                     .focusLostCallback(this::stopBlinking),
-                new LayoutBuilder((innerContext, constraints) -> {
-                    return new InstanceLocator(
-                        widgetInstance -> this.scrollableInstance = widgetInstance,
-                        new Scrollable(
-                            true, true,
-                            this.horizontalController,
-                            this.verticalController,
-                            new Constrain(
-                                Constraints.of(constraints.minWidth(), constraints.minHeight(), Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY),
-                                new InstanceLocator(
-                                    widgetInstance -> this.inputInstance = (TextInput.Instance) widgetInstance,
-                                    new TextInput(
-                                        this.widget().controller,
-                                        this.showCursor,
-                                        this.widget().softWrap,
-                                        this.widget().autoFocus,
-                                        this.widget().allowMultipleLines,
-                                        this.widget().baseStyle
-                                    )
-                                )
-                            )
-                        )
-                    );
-                })
+                new Scrollable(
+                    true, true,
+                    this.horizontalController,
+                    this.verticalController,
+                    new Builder(inputContext -> {
+                        this.inputContext = inputContext;
+                        return new TextInput(
+                            this.widget().controller,
+                            this.showCursor,
+                            this.widget().softWrap,
+                            this.widget().autoFocus,
+                            this.widget().allowMultipleLines,
+                            this.widget().baseStyle
+                        );
+                    })
+                )
             );
         }
     }
@@ -178,6 +157,6 @@ class InstanceLocator extends VisitorWidget {
 
     @Override
     public Proxy<?> proxy() {
-        return new VisitorWidget.Proxy<>(this, VISITOR);
+        return new Proxy<>(this, VISITOR);
     }
 }
