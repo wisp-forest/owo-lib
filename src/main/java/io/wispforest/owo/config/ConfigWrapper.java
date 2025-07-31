522ff4481089894f870342697f037d7e24c2902d
Fyn <nyfd2025@gmail.com>
2025-07-31 16:12:02 +0100

    Update ConfigWrapper.java
    Fix minor typo in message of thrown `IllegalStateException` exception

--- src/main/java/io/wispforest/owo/config/ConfigWrapper.java
+++ src/main/java/io/wispforest/owo/config/ConfigWrapper.java
@@ -99,7 +99,7 @@ public abstract class ConfigWrapper<C> {
 
         if (KNOWN_CONFIG_CLASSES.put(this.name, this.getClass()) != null) {
             throw new IllegalStateException("Config name '" + this.name + "'"
-                    + " is already taken an by instance of class '" + KNOWN_CONFIG_CLASSES.get(this.name).getName() + "'");
+                    + " is already taken by an instance of class '" + KNOWN_CONFIG_CLASSES.get(this.name).getName() + "'");
         }
 
         if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT && clazz.isAnnotationPresent(Modmenu.class)) {
