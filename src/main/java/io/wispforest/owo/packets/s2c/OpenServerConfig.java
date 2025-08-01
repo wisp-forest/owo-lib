0d9606c8a466657f9be1ae04ea10adbef7df1255
Dragon-Seeker <timekeeperguild@gmail.com>
2025-08-01 15:10:39 -0500

    Fix issue with not properly transferring parent screen from opening server config

--- src/main/java/io/wispforest/owo/packets/s2c/OpenServerConfig.java
+++ src/main/java/io/wispforest/owo/packets/s2c/OpenServerConfig.java
@@ -4,6 +4,7 @@ import blue.endless.jankson.JsonObject;
 import io.wispforest.endec.StructEndec;
 import io.wispforest.endec.impl.StructEndecBuilder;
 import io.wispforest.owo.Owo;
+import io.wispforest.owo.config.ui.ConfigScreen;
 import io.wispforest.owo.config.ui.ConfigScreenProviders;
 import io.wispforest.owo.network.ClientAccess;
 import io.wispforest.owo.serialization.endec.MinecraftEndecs;
@@ -22,7 +23,13 @@ public record OpenServerConfig(Identifier configId, JsonObject configData){
 
     @Environment(EnvType.CLIENT)
     public static void handle(OpenServerConfig packet, ClientAccess access) {
-        if (!ConfigScreenProviders.safelyOpenConfigScreen(packet.configId(), access.runtime().currentScreen, Map.of(packet.configId(), packet.configData()))) {
+        var parent = access.runtime().currentScreen;
+
+        if (parent instanceof ConfigScreen configScreen) {
+            parent = configScreen.parent;
+        }
+
+        if (!ConfigScreenProviders.safelyOpenConfigScreen(packet.configId(), parent, Map.of(packet.configId(), packet.configData()))) {
             Owo.LOGGER.warn("Unable to open the given config screen: {}", packet.configId());
         }
     }
