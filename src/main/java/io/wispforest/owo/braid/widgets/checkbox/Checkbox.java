4f0348d84a89bad5e1d4eaf148ce1f711257aff8
chyzman <jake@chezrothman.com>
2025-08-19 11:51:06 -0400

    Applied ControlsOverride to Checkboxes (and made them disableable)

--- src/main/java/io/wispforest/owo/braid/widgets/checkbox/Checkbox.java
+++ src/main/java/io/wispforest/owo/braid/widgets/checkbox/Checkbox.java
@@ -1,26 +1,32 @@
 package io.wispforest.owo.braid.widgets.checkbox;
 
+import blue.endless.jankson.annotation.Nullable;
 import io.wispforest.owo.braid.framework.BuildContext;
 import io.wispforest.owo.braid.framework.widget.StatelessWidget;
 import io.wispforest.owo.braid.framework.widget.Widget;
 import io.wispforest.owo.braid.widgets.SpriteWidget;
+import io.wispforest.owo.braid.widgets.basic.ControlsOverride;
+import io.wispforest.owo.braid.widgets.checkbox.RawCheckbox.CheckboxCallback;
 import net.minecraft.client.util.SpriteIdentifier;
 import net.minecraft.util.Identifier;
-
-import java.util.function.Consumer;
 
 public class Checkbox extends StatelessWidget {
 
     public final boolean checked;
-    public final Consumer<Boolean> onUpdate;
+    public final @Nullable CheckboxCallback onUpdate;
 
-    public Checkbox(boolean checked, Consumer<Boolean> onUpdate) {
+    public Checkbox(boolean checked, @Nullable CheckboxCallback onUpdate) {
         this.checked = checked;
         this.onUpdate = onUpdate;
     }
 
+    public Checkbox(boolean checked, CheckboxCallback onUpdate, boolean active) {
+        this(checked, active ? onUpdate : null);
+    }
+
     @Override
     public Widget build(BuildContext context) {
+        //TODO: visual indication of disabled/hovered states
         return new RawCheckbox(
             this.checked,
             this.onUpdate,
