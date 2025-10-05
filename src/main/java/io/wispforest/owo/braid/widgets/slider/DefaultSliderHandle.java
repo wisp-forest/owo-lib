5328a0f7fe86817ddc1f94cf44f05f38344e74f6
glisco <glisco03@web.de>
2025-10-06 00:55:42 +0200

    (braid) use `ButtonPanel` for default slider handle

--- src/main/java/io/wispforest/owo/braid/widgets/slider/DefaultSliderHandle.java
+++ src/main/java/io/wispforest/owo/braid/widgets/slider/DefaultSliderHandle.java
@@ -1,32 +1,18 @@
 package io.wispforest.owo.braid.widgets.slider;
 
 import io.wispforest.owo.braid.framework.BuildContext;
-import io.wispforest.owo.braid.framework.proxy.WidgetState;
-import io.wispforest.owo.braid.framework.widget.Key;
-import io.wispforest.owo.braid.framework.widget.StatefulWidget;
+import io.wispforest.owo.braid.framework.widget.StatelessWidget;
 import io.wispforest.owo.braid.framework.widget.Widget;
-import io.wispforest.owo.braid.widgets.basic.MouseArea;
-import io.wispforest.owo.braid.widgets.basic.Panel;
-import io.wispforest.owo.ui.component.ButtonComponent;
+import io.wispforest.owo.braid.widgets.basic.ControlsOverride;
+import io.wispforest.owo.braid.widgets.basic.EmptyWidget;
+import io.wispforest.owo.braid.widgets.button.ButtonPanel;
 
-public class DefaultSliderHandle extends StatefulWidget {
+public class DefaultSliderHandle extends StatelessWidget {
     @Override
-    public WidgetState<DefaultSliderHandle> createState() {
-        return new State();
-    }
-
-    public static class State extends WidgetState<DefaultSliderHandle> {
-
-        private boolean hovered = false;
-
-        @Override
-        public Widget build(BuildContext context) {
-            return new MouseArea(
-                widget -> widget
-                    .enterCallback(() -> setState(() -> this.hovered = true))
-                    .exitCallback(() -> setState(() -> this.hovered = false)),
-                new Panel(this.hovered ? ButtonComponent.HOVERED_TEXTURE : ButtonComponent.ACTIVE_TEXTURE).key(Key.of("slider-handle"))
-            );
-        }
+    public Widget build(BuildContext context) {
+        return new ButtonPanel(
+            !ControlsOverride.controlsDisabled(context),
+            EmptyWidget.INSTANCE
+        );
     }
 }
